9c6cc5271cf901ce07b218db8fe1898a1f115628
sindhuraedara <e.sindhura@gmail.com>
2022-06-17 20:35:45 +0530

    updated

--- DButils.java
+++ DButils.java
@@ -1,3 +1,5 @@
+hello this line is updated in dev branch
+
 Hello guys..
-this is the java file which contains the DB connection logiHello guys..
-this is the java file which contains the DB connection logicc
+
+this is the java file which contains the DB connection logic
